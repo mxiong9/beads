6f17b9725e8e5fee36a71976e6d6bca12b23acf7
Myra <mxiong@berkeley.edu>
2016-04-07 20:49:59 -0700

    first push

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+  $('li>a[id$="myButtons"]').click(function(event) {
+    event.preventDefault();
+    var href = $(this).attr('href');
+    alert("Are you sure you want to add a button" + href)
+  });
+
+  $('.addBead').click(function() { //toFix: beads overlap, adds to most recently added bead div. shift relationally
+    var destination = "";
+            if (num_beads == 0){
+                destination = document.getElementById("myContainer");
+            } else {
+                name = "bead" + String(num_beads);
+                destination = document.getElementById(name);
+            }            
+            var bead = document.createElement("DIV");
+            num_beads++;
+            bead.className = "bead";
+            bead.id = "bead" + String(num_beads);
+            destination.appendChild(bead);
+      
+    });
+
+});
+
+
