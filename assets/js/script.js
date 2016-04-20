5561d4b1e31ecbd67ff49dcf77453d8649bb603b
Myra <mxiong@berkeley.edu>
2016-04-20 11:05:46 -0700

    string

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,27 +6,36 @@ $(document).ready(function() {
     alert("Are you sure you want to add a button" + href)
   });
 
-  //Adding a Bead
+  //Adding a Bead and String
 
   $('#addBead').click(function() { //toFix: beads overlap, adds to most recently added bead div. shift relationally
     var destination = "";
     var num_beads = 0
-            if (num_beads == 0) {
-                destination = document.getElementById("myContainer");
-            } 
-            else {
-                name = "bead" + String(num_beads);
-                destination = document.getElementById(name);
-            }            
-            var bead = document.createElement("DIV");
-            num_beads++;
-            bead.className = "bead";
-            bead.id = "bead" + String(num_beads);
-            destination.appendChild(bead);
-      
-    });
+    if (num_beads == 0) {
+      destination = document.getElementById("myContainer");
+    }
+    else {
+      name = "bead" + String(num_beads);
+      destination = document.getElementById(name);
+    }
+    var bead = document.createElement("DIV");
+    num_beads++;
+    bead.className = "bead";
+    bead.id = "bead" + String(num_beads);
+    destination.appendChild(bead);
 
+    var stringDestination = "";
+    var offset = $(".bead").offset().left;
+    console.log(offset);
+    var numStrings = 0;
+    if (offset <= 60) {
+      name = "necklaceString" + String(numStrings);
+      stringDestination = document.getElementById(name);
+      //stringDestination = document.getElementById("myContainer");
+      numStrings++;
+    }
+    var necklaceString = document.createElement("DIV");
+    stringDestination.appendChild(necklaceString);
 
+  });
 });
-
-
