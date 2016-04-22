0607f1041fac618f32c50372f7a3e12de0312727
Myra <mxiong@berkeley.edu>
2016-04-21 21:24:23 -0700

    text add

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,22 +21,30 @@ $(document).ready(function() {
     bead.id = "bead" + String(num_beads);
     destination.appendChild(bead);
 
-    var stringDestination = "";
-    var offset = $(".bead").offset().left;
-    console.log(offset);
-    var numStrings = 0;
-    if (offset <= 60) {
-      name = "necklaceString" + String(numStrings);
-      stringDestination = document.getElementById(name);
+ //   var stringDestination = "";
+   // var offset = $(".bead").offset().left;
+  //  console.log(offset);
+   // var numStrings = 0;
+    //if (offset <= 60) {
+   //   name = "necklaceString" + String(numStrings);
+    //  stringDestination = document.getElementById(name);
       //stringDestination = document.getElementById("myContainer");
-      numStrings++;
-    }
-    var necklaceString = document.createElement("DIV");
-    stringDestination.appendChild(necklaceString);
-
-    var text = $('#text').val();
+     // numStrings++;
+  //  }
+   // var necklaceString = document.createElement("DIV");
+   // stringDestination.appendChild(necklaceString);
 
 
+
+ //   var content = $('#text').val();
+ //   content.id = "content" + String(num_beads);
+
+ //   content.appendTo(bead);
+
+    var content = document.getElementById('text').value;
+    document.getElementById(bead.id).innerHTML = content;
+//problem: hard to move the text without moving the bead because it's the same DIV
+    content.val('');
 
   });
 });
