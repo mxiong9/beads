43c4f06847ca7c17e53e91c2d34a886d9dbfb750
okchip <ameena.golding@googlemail.com>
2016-04-21 21:45:07 -0700

    Fix string, working version

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
-var num_beads = 0
+var num_beads = 0; 
+// var numStrings = 0; //not currently counting strings
+
 
 $(document).ready(function() {
 
@@ -10,67 +12,43 @@ $(document).ready(function() {
 
   //Adding a Bead and String
 
-    var num_beads = 0
-
-  $('#addBead').click(function() { //toFix: beads overlap, adds to most recently added bead div. shift relationally
-    var destination = "";
-<<<<<<< HEAD
-            if (num_beads == 0) {
-                var destination = document.getElementById("myContainer");
-            } 
-            else {
-                name = "bead" + String(num_beads);
-                destination = document.getElementById(name);
-            }            
-            var bead = document.createElement("DIV");
-            num_beads++;
-            bead.className = "bead";
-            bead.id = "bead" + String(num_beads);
-            destination.appendChild(bead);
-            var necklacestring = document.createElement("necklacestring");
-
-
-            var screenWidth = $(window).width();
-            if (num_beads == 13) {
-                destination.appendChild(necklacestring);
-
-
-            }
-
-      
-    });
-=======
-    
+  $('#addBead').click(function() { //toFix: beads overlap, adds to most recently added bead div. shif relationally
+    var lastBead; //the last bead div added to the html file
+    var windowProportion;
+    var beadSize = 60; //will eventually get the information from the css   
+    var windowWidth = $(window).width(); 
     var destination = document.getElementById("myContainer");
->>>>>>> c2ea986d5a4e9d5c3291830d01a429ac17256203
 
+    if ($('.bead').length != 0){ //if there are beads already on the page
+        lastBead = $( ".bead" ).last(); 
+        var lastOffset = lastBead.offset().left;
+        windowProportion = (lastOffset + beadSize)/windowWidth; //temp solution
+        console.log("lastOffset", lastOffset);
+    } else {
+        windowProportion = 0;
+    } 
+
+    /*when the amount of the screen taken by adding a bead
+     exceeds 90% (only works on some sizes), add a new string*/
+    if (windowProportion > 0.9){ 
+        addString();
+    } 
+  
+    /*add the new bead*/
     var bead = document.createElement("DIV");
     num_beads++;
     bead.className = "bead";
     bead.id = "bead" + String(num_beads);
-    destination.appendChild(bead);
-
-<<<<<<< HEAD
-
-});
-=======
-    var stringDestination = "";
-    var offset = $(".bead").offset().left;
-    console.log(offset);
-    var numStrings = 0;
-    if (offset <= 60) {
-      name = "necklaceString" + String(numStrings);
-      stringDestination = document.getElementById(name);
-      //stringDestination = document.getElementById("myContainer");
-      numStrings++;
-    }
-    var necklaceString = document.createElement("DIV");
-    stringDestination.appendChild(necklaceString);
->>>>>>> c2ea986d5a4e9d5c3291830d01a429ac17256203
-
-    var text = $('#text').val();
-
-
-
+    destination.appendChild(bead); 
+    console.log("num_beads", num_beads);     
   });
 });
+
+function addString(){
+    var destination = document.getElementById("myContainer");
+    var string = document.createElement("DIV");
+    destination.appendChild(string);
+    // string.name = "necklaceString" + String(numStrings);
+    string.className = "necklaceString";
+    // numStrings++; //not currently counting strings    
+}
